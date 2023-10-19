fa1078611e11b53a3a66a9a7b0c6bf9ebff45aec
Solano <hsolano@weathertop.cs.middlebury.edu>
2023-10-19 15:40:19 -0400

    Starting

---
+++ myio.c
@@ -0,0 +1,25 @@
+/*myio.c*/
+
+# define BUFFER_SIZE 4096;
+
+#include <stdio.h>
+
+/*Function prototypes*/
+
+FILE *myopen(const char *restrict pathname, const char *restrict flags, int BUFFER_SIZE);
+
+
+
+
+int main(int argc, char *argv[]) {
+
+}
+
+
+
+
+
+FILE *myopen(const char *restrict pathname, const char *restrict mode, int BUFFER_SIZE) {
+    //intialize flags
+    //
+}
